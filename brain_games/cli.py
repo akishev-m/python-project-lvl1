91988849af2c1f6a3625dd6bb0da49dbfd37828c
akishev-ma <mikhail.akishev@gmail.com>
2022-03-15 19:29:15 +0500

    mistakes correction

---
+++ brain_games/cli.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import scripts.brain_games
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    scripts.brain_games.welcome_user()
+   
+#    print(sys.path)
+
+if __name__ == '__main__':
+    main()
+#    brain_games.welcome_user()
+
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
+import prompt
+
+def welcome_user():
+    name = ''
+    while name == '':
+        name = prompt.string('May I have your name? ')
+    print('Hello, ' + name + '!')
+
+
 def main():
-    print("Welcome to the Brain Games!")
+    print("Welcome to the Brain Games!") 
 
 if __name__ == '__main__':
     main()
+
