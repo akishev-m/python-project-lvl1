2793d44400e916b9d6d7e58181ba39c71f51fafa
akishev-m <mikhail.akishev@gmail.com>
2022-03-28 13:29:58 +0500

    linter workflow added

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,15 +2,17 @@
 
 import prompt
 
+
 def welcome_user():
     name = ''
     while name == '':
         name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
 
+
 def main():
     welcome_user()
 
+
 if __name__ == '__main__':
     main()
-
--- brain_games/scripts/BRAIN_GAMES.py
+++ brain_games/scripts/BRAIN_GAMES.py
@@ -2,10 +2,11 @@
 
 import brain_games.cli
 
-def main():    
-    print("Welcome to the Brain Games!") 
+
+def main():
+    print("Welcome to the Brain Games!")
     brain_games.cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
-
